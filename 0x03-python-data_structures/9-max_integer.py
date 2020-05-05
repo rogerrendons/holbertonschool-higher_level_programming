fc0f5760b463cde47dec4d572b6b09189f785fd8
Roger Rendon <1584@holbertonschool.com>
2020-05-05 18:11:22 -0500

    task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max = 0
+    MV = 0
     if (len(my_list) == 0):
         return ("None")    
     for rec in range(len(my_list)):
-        if max < my_list[rec]:
-            max = my_list[rec]
-    return (max)
+        if MV < my_list[rec]:
+            MV = my_list[rec]
+    return (MV)
