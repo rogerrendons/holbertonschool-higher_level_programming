bf6b32fa2d328129453da5ac435a9df022a86e8a
Roger Rendon <1584@holbertonschool.com>
2020-05-05 19:13:10 -0500

    task 11 y 12

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    result = []
-    for rec in my_list:
-        if rec % 2 == 0:
-            result.append(True)
+    resul = []
+    for reco in my_list:
+        if reco % 2 is 0:
+            resul.append(True)
         else:
-            result.append(False)
-        return result
+            resul.append(False)
+    return resul
