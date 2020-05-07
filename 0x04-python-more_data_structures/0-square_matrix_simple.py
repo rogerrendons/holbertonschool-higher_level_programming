2d4539ad313eef675d1bc50cf6bf21bce1d89501
Roger <1584@holbertonschool.com>
2020-05-07 12:15:41 -0500

    task 0

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    if matrix is not None:
+        print(len(matrix))
+	    #return [[element**2 for element in matrix[i]] for i in range(len(matrix))]
