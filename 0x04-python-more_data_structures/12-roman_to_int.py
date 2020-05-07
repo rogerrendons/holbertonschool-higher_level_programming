4f4329d11113e5e8ba804cffc0ff00a41a1e0062
Roger <1584@holbertonschool.com>
2020-05-07 18:29:49 -0500

    task 10 to 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,12 +3,11 @@ def roman_to_int(roman_string):
     result = 0
     if type(roman_string) == str:
         largest = 1
-        roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        r = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         for read in roman_string[::-1]:
-#           if read in roman:
-            if roman[read] >= largest:
-                result = result + roman[read]
-                largest = roman[read]
+            if r[read] >= largest:
+                result = result + r[read]
+                largest = r[read]
             else:
-                result = result - roman[read]
+                result = result - r[read]
     return result
