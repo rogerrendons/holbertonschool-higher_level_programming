3f93e7539012d6917b931bca9e994c932da77188
Roger <1584@holbertonschool.com>
2020-05-07 15:20:45 -0500

    task 4

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
     if (set_1 and set_2) is not None:
-        return (set.union(set_1, set_2))
+        return (set.symmetric_difference(set_1, set_2))
