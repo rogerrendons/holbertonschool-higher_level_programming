93c2db2c65ac6f448949e9ae180a6a076997a212
Roger <1584@holbertonschool.com>
2020-05-07 14:07:15 -0500

    task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if my_list is None:
+    if my_list is not None:
         return [replace if num == search else num for num in my_list]
