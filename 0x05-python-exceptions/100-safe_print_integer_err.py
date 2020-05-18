18cdd26e3c1dd1910ba5e10e35a935a4ffe25553
Roger <1584@holbertonschool.com>
2020-05-18 18:00:37 -0500

    Solution Task advanced 7 and 8

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 import sys
 
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
+        return True
     except Exception as error:
-        print("Exception: ", error, file=sys.stderr)
+        print("Exception: {:s}".format(str(error)), file=sys.stderr)
         return False
     else:
         return True
--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -2,9 +2,8 @@
 import sys
 
 def safe_function(fct, *args):
+    
     try:
         return fct(*args)
     except Exception as ddd:
         print("Exception:", ddd, file=sys.stderr)
-
-
