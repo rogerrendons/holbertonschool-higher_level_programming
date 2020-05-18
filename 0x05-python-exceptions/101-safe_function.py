aef6b79cde53e1574ec54ec0b8047a5a0245046e
Roger <1584@holbertonschool.com>
2020-05-18 17:53:37 -0500

    Solution Task advanced 7 and 8

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+
+def safe_function(fct, *args):
+    try:
+        return fct(*args)
+    except Exception as ddd:
+        print("Exception:", ddd, file=sys.stderr)
+
+
