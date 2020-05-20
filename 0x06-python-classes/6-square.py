a79742772deed21dde480ae481de65a5ab24e246
Roger <1584@holbertonschool.com>
2020-05-19 21:33:34 -0500

    Upload Task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -37,7 +37,7 @@ class Square:
                 print()
             for a in range(self.size):
                 for b in range(self.__position[0]):
-                    print('_', end='')
+                    print(' ', end='')
                 for c in range(self.__size):
                     print('#', end='')
                 print()
