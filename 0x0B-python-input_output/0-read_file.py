1f897cae4c3f8b6f892d427be884ef8b99da8bdc
Roger <1584@holbertonschool.com>
2020-06-03 13:02:21 -0500

    Task 1

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Function read_file """
+""" Write a function that reads a text file (UTF8) and prints it to stdout """
 
 
 def read_file(filename=""):
---
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Write a function that returns the number of lines of a text file """
+def number_of_lines(filename=""):
+    Counter = 0
+    with open(filename, mode="r", encoding='utf-8') as file:
+        for line in file:
+            Counter += 1
+    return Counter
