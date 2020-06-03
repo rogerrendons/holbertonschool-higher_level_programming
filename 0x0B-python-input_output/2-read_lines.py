96fab37ff86ec047649afeb83e6928c92abfcd80
Roger <1584@holbertonschool.com>
2020-06-03 13:36:21 -0500

    Task 2

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Write a function that reads n lines of
+    a text file (UTF8) and prints it to stdout
+"""
+
+
+def read_lines(filename="", nb_lines=0):
+    """ Read lines """
+    Counter = 0
+    with open(filename, mode="r", encoding='utf-8') as file:
+        if nb_lines <= 0:
+            print(file.read(), end='')
+        else:
+            for line in range(nb_lines):
+                if (Counter < nb_lines):
+                    print(line, end="")
+                    Counter += 1
