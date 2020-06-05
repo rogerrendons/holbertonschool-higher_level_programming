7bbf73d07a7f2c36cf53d9e4518fd52c46b801ed
Roger <1584@holbertonschool.com>
2020-06-04 19:51:30 -0500

    fix 6

--- 0x0B-python-input_output/6-from_json_string.py
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -3,8 +3,10 @@
     Write a function that returns an object
     (Python data structure) represented by a JSON string
 """
-import json
 
 
 def from_json_string(my_str):
+    """ Return Object """
+
+    from json import loads
     return json.loads(my_str)
