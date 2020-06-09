924cd4f64ce2acc2db87195fdf0c5f9657dc72ea
Roger <1584@holbertonschool.com>
2020-06-09 15:18:10 -0500

    Upload base 1

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+""" initial base.py """
 import json
 
 
 class Base:
     """ Define module class Base """
 
-    __nb_objects = 0    # Private class atributte#
+    __nb_objects = 0
 
     def __init__(self, id=None):
         """ Base instance, constructor class"""
@@ -14,6 +15,32 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @classmethod
+    def create(cls, **dictionary):
+        """ return atributes in dictionary """
+        # from models.rectangle import Rectangle
+        # from models.square import Square
+        if cls.__name__ == "Rectangle":
+            figure = cls(16, 7)
+        elif cls.__name__ == "Square":
+            figure = cls(7)
+        return (figure.update(**dictionary))
+
+    @classmethod
+    def load_from_file(cls):
+        """ load from a file an object """
+        load = []
+        try:
+            with open(cls.__name__ + '.json', mode='r', encoding='utf-8') as\
+                 file:
+                listres = json.dumps(json.load(file))
+        except FileNotFoundError:
+            return load
+        list_dictionaries = cls.from_json_string(listres)
+        for rec in list_dictionaries:
+            load.append(cls.create(**rec))
+        return load
 
     @staticmethod
     def to_json_string(list_dictionaries):
@@ -30,29 +57,3 @@ class Base:
             return json.loads(json_string)
         return []
 
-    @classmethod
-    def create(cls, **dictionary):
-        """ return atributes in dictionary """
-        from models.rectangle import Rectangle
-        from models.square import Square
-        if cls.__name__ == "Rectangle":
-            figure = Rectangle(16, 7)
-        elif cls.__name__ == "Square":
-            figure = Square(7)
-        figure.update(**dictionary)
-        return figure
-
-    @classmethod
-    def load_from_file(cls):
-        """ load from a file an object """
-        load = []
-        try:
-            with open(cls.__name__ + '.json', mode='r', encoding='utf-8') as\
-                 file:
-                listres = json.dumps(json.load(file))
-        except FileNotFoundError:
-            return load
-        list_dictionaries = cls.from_json_string(listres)
-        for rec in list_dictionaries:
-            load.append(cls.create(**rec))
-        return load
