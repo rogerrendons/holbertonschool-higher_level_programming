6de8c1016c2c34b1cfa5efcf586c83f6aedfc5cf
Roger Rendon <1584@holbertonschool.com>
2020-07-13 18:54:52 -0500

    Task 7

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -3,6 +3,4 @@ INSERT INTO first_table VALUES ('89', 'Holberton School');
 
 -- INSERT INTO first_table (id, name) VALUES ('89', 'Holberton School');
 
--- INSERT INTO first_table(id, name) values (89, "Holberton School");
-
 -- INSERT INTO first_table (id, name) VALUES (89, 'Holberton School');
