7945eb5e6e6fefd4e26519de35bdaf4fa345d494
Roger Rendon <1584@holbertonschool.com>
2020-07-14 18:53:57 -0500

    Task 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- Show cities 
 -- in the database
-SELECT cities.id, cities.name, states.name FROM cities JOIN states ON states.state_id = cities.state_id
-ORDER by cities.id ASCC;
+SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = states.id
+ORDER by cities.id ASC;
