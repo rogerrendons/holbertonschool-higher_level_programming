15572bb6438a62f3cf2728f79ce0906014112fab
Roger Rendon <1584@holbertonschool.com>
2020-07-13 20:30:43 -0500

    Task 15 and 16

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,2 @@
+-- Show the same score in second_table
+SELECT score, COUNT(*) as number FROM second_table GROUP BY score ORDER BY number DESC;
---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,2 @@
+-- Show records of second_table has a value
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
