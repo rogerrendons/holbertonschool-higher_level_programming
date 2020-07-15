37ee1aa84ab390680aa2f58321ffdb5eaf86e7e1
Roger Rendon <1584@holbertonschool.com>
2020-07-14 23:09:03 -0500

    task 16

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,3 +1,4 @@
--- Show all genres in a show
+-- Show genres 
+-- in show
 SELECT tv_shows.title, tv_genres.name FROM tv_genres RIGHT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 RIGHT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id ORDER by tv_shows.title;
