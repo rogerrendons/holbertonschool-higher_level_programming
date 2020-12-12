ae33ba5e5bf2c93cee31a17a74694f28512c3a3e
rogerrendons <1584@holbertonschool.com>
2020-12-12 17:49:18 -0500

    Correction task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
-"""
-"""
+""" Wait, do you remember the previous task? Did you test "Arizona'; TRUNCATE
+    TABLE states ; SELECT * FROM states WHERE name = '" as an input? """
+
+import MySQLdb
+import sys
+
 
 if __name__ == "__main__":
-    import MySQLdb
-    from sys import argv
+    cnx = MySQLdb.connect(host="localhost", port=3306, user="{}"
+                          .format(sys.argv[1]), passwd="{}"
+                          .format(sys.argv[2]), db="{}"
+                          .format(sys.argv[3]),
+                          charset="utf8")
+    cur = cnx.cursor()
+    cur.execute("SELECT * FROM states WHERE BINARY name=%s", [sys.argv[4]])
 
-    with MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
-                         db=argv[3], port=3306) as db:
-        db.execute("SELECT *\
-                   FROM states\
-                   WHERE name = %s", (argv[4],))
-        table = db.fetchall()
-        for data in table:
-            print(data)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    cnx.close()
