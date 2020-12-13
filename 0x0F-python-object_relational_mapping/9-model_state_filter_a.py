a41665bd6770ef636367f80e14e69c47abc3ed35
rogerrendons <1584@holbertonschool.com>
2020-12-13 11:01:45 -0500

    Corrections 2

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -9,11 +9,12 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
-                           format(argv[1], argv[2], argv[3])                           )
+                           format(argv[1], argv[2], argv[3]))
+    
     Base.metadata.create_all(engine)
     session = Session(engine)
 
-    for data in session.query(State).filter(State.name.like
+    for data in session.query(State).filter(State.name.like\
                                             ("%a%")).order_by(State.id):
         print("{}: {}".format(data.id, data.name))
     session.close()
