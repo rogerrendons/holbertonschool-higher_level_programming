073c6674650f1d627ff64edc50490cbeb808b3ee
rogerrendons <1584@holbertonschool.com>
2020-12-13 11:48:25 -0500

    Corrections 8

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -6,14 +6,13 @@ from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from model_state import Base, State
-from sqlalchemy import literal
+
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
-                           format(argv[1], argv[2], argv[3]),
-                           )
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                            argv[1], argv[2], argv[3]))
     Base.metadata.create_all(engine)
-    session = Session()
+    session = Session(engine)
 
     q = session.query(State).filter(State.id == 2)
     record = q.one()
