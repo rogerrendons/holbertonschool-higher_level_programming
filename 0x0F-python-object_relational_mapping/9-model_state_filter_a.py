0d257df75799bc01e333f5e506ca9507a282f029
rogerrendons <1584@holbertonschool.com>
2020-12-13 11:06:56 -0500

    Corrections 4

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
+"""Start link class to table in database
 """
-"""
-
 from sys import argv
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
-from model_state import Base, State
+
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
-                           format(argv[1], argv[2], argv[3]))
-    
-    Base.metadata.create_all(engine)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                            argv[1], argv[2], argv[3]))
     session = Session(engine)
-
     for state in session.query(State).filter(State.name.like("%a%")).all():
         print("{}: {}".format(state.id, state.name))
     session.close()
