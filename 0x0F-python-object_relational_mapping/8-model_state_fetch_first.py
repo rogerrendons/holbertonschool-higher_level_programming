cb8c38e7691efca4c149b0e30f966b83bf7fdc9b
rogerrendons <1584@holbertonschool.com>
2020-12-13 10:49:33 -0500

    Corrections 2

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
-"""
+""" Class in database
 """
 
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sys import argv
 from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(sys.argv[1], sys.argv[2],
-                                  sys.argv[3]),
-                           )
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    ses = Session()
-    data = ses.query(State).first()
-
-    if data:
-        print("{}: {}".format(data.id, data.name))
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'. format(
+                            argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    session = Session(engine)
+    state = session.query(State).order_by(State.id).first()
+    if state:
+        print("{}: {}".format(state.id, state.name))
     else:
-        print("Nothing")
+        print("nothing")
     session.close()
