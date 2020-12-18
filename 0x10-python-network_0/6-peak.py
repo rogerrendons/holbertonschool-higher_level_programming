26736af1153280616f930ddfb4ea0f2356e5177e
rogerrendons <1584@holbertonschool.com>
2020-12-18 15:45:27 -0500

    Task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-""" Find unsorted integers peak """
+""" Find peak in unsorted integers """
 
 def find_peak(list_of_integers):
+    ''' Find Peak Class. '''
+
     if list_of_integers:
-        for First, Last in enumerate(list_of_integers):
-            if First > 1:
-                if Last < list_of_integers[First-1]:
-                    if list_of_integers[First-2] < list_of_integers[First-1]:
-                            return list_of_integers[First-1]
         return max(list_of_integers)
-    else:
-        return
+    return None
