87cc4b38eb866f5439023355f4157da523dbf48b
rogerrendons <1584@holbertonschool.com>
2020-12-18 17:47:56 -0500

    Load Correction Task 5 V1

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,6 +4,12 @@
 def find_peak(list_of_integers):
     ''' Find Peak Class. '''
 
+    lst = []
+    lst = list_of_integers
     if list_of_integers:
-        return max(list_of_integers)
+        if len(list_of_integers) % 3 == 0:
+            lst.pop(0)
+            return max(lst)
+        else:
+            return max(lst)
     return None
