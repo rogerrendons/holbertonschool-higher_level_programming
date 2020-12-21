ec45b0a8343d802906eb9e60b5f08a82694b652d
rogerrendons <1584@holbertonschool.com>
2020-12-21 14:44:09 -0500

    correct 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 """ Find peak in unsorted integers """
 
-def find_peak(list_of_integers):
-    ''' Find Peak Class. '''
 
-    
-    if list_of_integers:
-        if len(list_of_integers) % 3 == 0:
-            list_of_integers.pop(0)
-            return max(list_of_integers)
-        else:
-            return max(list_of_integers)
-    return None
+def find_peak(list_of_integers):
+    """function find integers"""
+
+    if len(list_of_integers) == 0:
+        return None
+    lists, lng = list_of_integers, len(list_of_integers)
+    midt = lng // 2
+    if (midt == lng - 1 or lists[midt] >= lists[midt + 1]):
+        if (midt == 0 or lists[midt] >= lists[midt - 1]):
+            return lists[midt]
+    if midt != lng - 1 and lists[midt + 1] > lists[midt]:
+        return find_peak(lists[midt + 1:])
+    return find_peak(lists[:midt])
