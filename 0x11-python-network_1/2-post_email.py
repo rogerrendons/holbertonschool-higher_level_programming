5c97f7929a519e250139883db19bddedb9b7699d
rogerrendons <1584@holbertonschool.com>
2021-01-11 14:50:11 -0500

    Correction 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,11 +1,12 @@
-# #!/usr/bin/python3
-# """ Python Script Send POST Request """
+#!/usr/bin/python3                                                                
+""" Python Script Send POST Request """
 
 if __name__ == "__main__":
     import sys
     from urllib import request, parse
-    mail = parse.urlencode({'email': sys.argv[2]})
-    mail = mail.encode('utf-8')
-    with request.urlopen(sys.argv[1], mail) as response:
-            mail = response.read().decode('utf-8')
-            print(mail)
+
+    Mail = parse.urlencode({'email': sys.argv[2]})
+    Mail = Mail.encode('utf-8')
+    with request.urlopen(sys.argv[1], Mail) as response:
+        Mail = response.read().decode('utf-8')
+        print(Mail)
