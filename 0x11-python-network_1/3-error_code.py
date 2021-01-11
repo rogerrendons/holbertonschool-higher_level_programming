d9a7cc2ca2d7252a7cc8116f7639099f69f493d8
rogerrendons <1584@holbertonschool.com>
2021-01-11 17:28:10 -0500

    Task 3.5

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python3                                                                
-""" Write a Python script that takes in a URL, sends a                            
-request to the URL and displays the body of the                                   
+#!/usr/bin/python3
+""" Write a Python script that takes in a URL, sends a
+request to the URL and displays the body of the
 response (decoded in utf-8)."""
 
 import sys
@@ -12,4 +12,4 @@ if __name__ == "__main__":
         with request.urlopen(sys.argv[1]) as response:
             print(response.read().decode('utf-8'))
     except error.HTTPError as e:
-        print("Error code:",e.code)
+        print("Error code:" ,e.code)
