55dcf22e617ec5a7fa4132eeacde6bc9db9da8d2
rogerrendons <1584@holbertonschool.com>
2021-01-11 22:09:37 -0500

    Jueputa Dio

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Please list 10 commits (from the most recent to oldest)
+of the repository “rails” by the user “rails” """
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    result = requests.get("https://api.github.com/repos/{}/{}/commits".
+                          format(sys.argv[2], sys.argv[1]))
+    res = result.json()
+
+for x in range(10):
+    print("{}: {}".format(res[x].get("sha"), res[x].
+          get("commit").get("author").get("name")))
