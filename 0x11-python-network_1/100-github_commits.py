4281e1f6082c6791e986f4311de11d2bb0aa87db
rogerrendons <1584@holbertonschool.com>
2021-01-11 22:39:09 -0500

    To correct 2

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,6 +6,9 @@ import requests
 import sys
 
 if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        sys.exit()
+
     result = requests.get("https://api.github.com/repos/{}/{}/commits".
                           format(sys.argv[2], sys.argv[1]))
     res = result.json()
