4c4ff205b82f1ea6e5f6ff81fc18e80dbbd477c9
rogerrendons <1584@holbertonschool.com>
2021-01-11 19:05:45 -0500

    Task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Python script that takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    q = ''
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+
+    try:
+        result = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+        result.json()
+        if 'id' in result and 'name' in result:
+            print('[{}] {}'.format(result['id'], result['name']))
+        else:
+            print('No result')
+    except ValueError:
+        print('Not a valid JSON')
