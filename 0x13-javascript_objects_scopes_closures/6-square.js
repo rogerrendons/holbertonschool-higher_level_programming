7b2e59930530e3aa95370b16a45eeac0a63d4ffc
rogerrendons <1584@holbertonschool.com>
2021-02-08 14:46:55 -0500

    Task 5

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,12 +2,12 @@
 const XSquare = require('./5-square.js');
 
 module.exports = class Square extends XSquare {
-    charPrint(C){
-    if (C == null){
-        C = "X"
+  charPrint (C) {
+    if (C == null) {
+      C = 'X';
     }
     for (let i = 0; i < this.height; i++) {
-        console.log(C.repeat(this.width));
-      }
+      console.log(C.repeat(this.width));
+    }
   }
 };
