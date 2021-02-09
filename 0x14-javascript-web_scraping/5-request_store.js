b382e841dce4a6bdcd4958a07b5c46c33e11dbba
rogerrendons <1584@holbertonschool.com>
2021-02-09 16:20:18 -0500

    Task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -7,11 +7,10 @@ request(process.argv[2], (err, resp, body) => {
   if (err) {
     console.error(err);
   } else {
-        myFile.writeFile(process.argv[3], body, 'utf-8', (err, data) => {
-            if (err) {
-                console.log(err);
-            }
-        });
-
+    myFile.writeFile(process.argv[3], body, 'utf-8', (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+    });
   }
 });
