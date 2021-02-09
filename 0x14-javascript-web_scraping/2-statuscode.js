d280faff90300ec91353f278eaf84bc6de32483b
rogerrendons <1584@holbertonschool.com>
2021-02-09 13:08:21 -0500

    Task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,7 +4,7 @@ const request = require('request');
 
 request(process.argv[2], (err, resp) => {
   if (err) {
-    console.error(err);
+    console.error('code: ', err);
   } else {
     console.log('code:', resp && resp.statusCode);
   }
