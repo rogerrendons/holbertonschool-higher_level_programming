2889d3f09de6e984c96c101695c90dd7f0806b34
rogerrendons <1584@holbertonschool.com>
2021-02-09 13:49:47 -0500

    Task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,10 +2,10 @@
 
 const request = require('request');
 
-request("https://swapi-api.hbtn.io/api/films/" + process.argv[2], (err, resp, body) => {
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], (err, resp, body) => {
   if (err) {
     console.error(err);
-    } else {
-      console.log(JSON.parse(body).title);
-    }
+  } else {
+    console.log(JSON.parse(body).title);
+  }
 });
