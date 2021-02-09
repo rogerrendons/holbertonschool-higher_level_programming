6658aa11ce4ccb560daa99d30d19b21020e9431f
rogerrendons <1584@holbertonschool.com>
2021-02-09 13:43:17 -0500

    Task 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request("https://swapi-api.hbtn.io/api/films/" + process.argv[2], (err, resp, body) => {
+  if (err) {
+    console.error(err);
+    } else {
+      console.log(JSON.parse(body).title);
+    }
+});
