a39fecedcd7ef4d10fe42f7438f80e077d264855
rogerrendons <1584@holbertonschool.com>
2021-02-09 14:46:35 -0500

    Task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,18 +4,18 @@ const request = require('request');
 const args = process.argv;
 let Acum = 0;
 
-request(args[2], function(err, resp, body) {
-    if (err) {
-        console.log(err);
-    }else{
-        const films = JSON.parse(body).results;
-        for (let List in films){
-            for (let Charac in films[List].characters){
-                if (films[List].characters[Charac] === 'https://swapi-api.hbtn.io/api/people/18/') {
-                    Acum++;
-                }
-            }
+request(args[2], function (err, resp, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const films = JSON.parse(body).results;
+    for (const List in films) {
+      for (const Charac in films[List].characters) {
+        if (films[List].characters[Charac] === 'https://swapi-api.hbtn.io/api/people/18/') {
+          Acum++;
         }
+      }
     }
-    return console.log(Acum);
+  }
+  return console.log(Acum);
 });
