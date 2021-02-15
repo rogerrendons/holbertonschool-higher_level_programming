cd0d569ece4f977c51a62110e6e3702b3285fa4c
rogerrendons <1584@holbertonschool.com>
2021-02-15 17:46:26 -0500

    Upload Files

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,5 @@
+$.getJSON("https://swapi-api.hbtn.io/api/films/?format=json", function(data){
+  for (let i in data.results){
+    $('UL#list_movies').append('<li>' + data.results[i].title + '</li>');
+  }
+});
